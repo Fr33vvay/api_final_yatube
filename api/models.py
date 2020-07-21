d045d79e3e3fb1a03f1fa7c9dac61a3fb6aa2569
Илья <7634216@gmail.com>
2020-07-21 23:50:29 +0300

    уникальность пар в модели подписки

--- api/models.py
+++ api/models.py
@@ -43,3 +43,6 @@ class Follow(models.Model):
     following = models.ForeignKey(User, on_delete=models.CASCADE,
                                   verbose_name='Автор',
                                   related_name='following')
+
+    class Meta:
+        unique_together = ('user', 'following')
